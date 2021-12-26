499a1e886f5c9fbe248a79d33586f9afbd5fafb1
tiny-engineery <90619504+tiny-engineery@users.noreply.github.com>
2021-12-26 14:46:48 +0300

    Star
    Added class Star Platinum in 2D

--- Attempts/Solar_System.py
+++ Attempts/Solar_System.py
@@ -5,3 +5,20 @@ Created on Sun Dec 26 14:16:23 2021
 @author: grego
 """
 
+class Star(mass: float):
+    """
+    Class desribes star. 
+    Situated in (0,0) and has solar mass, if doesn't mentioned another
+    """
+    def __init__(self, mass = 10^30, xy = (0, 0)):
+        self.mass = mass
+        self.xyz = xy
+    
+    def getMass(self):
+        return self.mass
+    
+    def getPosition(self):
+        return self.xy
+
+
+    

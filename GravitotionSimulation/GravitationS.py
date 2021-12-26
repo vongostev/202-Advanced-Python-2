a12598004de2fce77482b742d82bfd9cdd184604
Belashov641618 <90342122+Belashov641618@users.noreply.github.com>
2021-12-26 14:37:56 +0300

    Новые способы добавления объектов в систему
    Так как случайным образом, добавляя объекты в систему довольно сложно было вычислить их параметры, чтобы они стали частью этой системы, не упали в центр, приводя к коллапсу всей системы и не улетали на бесконечность.
    
    Поэтому были добавлены два новых метода:
    1. Добавление объекта, указываю его период и коэффициент алфьфа:
       Важно что при таком способе добавления тела коэффициент отвечает за отличие его скорости от максимальной возможной для удержания тела на орбите.
    2 Добавлен способ добавить тело с использованием коэффициента бэта, который по сути отвечает за округлость орбиты. Если коэффициент равен 1 - то орбиты максимально круглая

--- GravitotionSimulation/GravitationS.py
+++ GravitotionSimulation/GravitationS.py
@@ -7,8 +7,22 @@ fig = plt.figure(figsize=(10, 10), dpi=100)
 ax = fig.add_subplot(111, projection='3d')
 camera = Camera(fig)
 
+
 from random import random
 import numpy
+
+
+import math
+
+def dotproduct(v1, v2):
+  return sum((a*b) for a, b in zip(v1, v2))
+
+def length(v):
+  return math.sqrt(dotproduct(v, v))
+
+def angle(v1, v2):
+  return math.acos(dotproduct(v1, v2) / (length(v1) * length(v2)))
+
 
 
 SimulationSettings = {}
@@ -103,6 +117,14 @@ class Object:
         
         return VecForce
         
+    
+    def data(self):
+        print("ObjectBox ", self, " - has been sucessfully created with:")
+        print("\tPosition: \t{", self.pos[0], ",", self.pos[1], ",", self.pos[2], "} :", numpy.linalg.norm(self.pos))
+        print("\tVelocity: \t{", self.spd[0], ",", self.spd[1], ",", self.spd[2], "} :", numpy.linalg.norm(self.spd))
+        print("\tSelfAxis: \t{", self.axs[0], ",", self.axs[1], ",", self.axs[2], "} :", numpy.linalg.norm(self.axs))
+        print("\tMass: \t\t\t", self.mas)
+        print("\tSelfRadius: \t", self.rad)
 
 class ObjectSystem:
     
@@ -139,6 +161,92 @@ class ObjectSystem:
             temp.rad = dic.get("rad")
             
         self.add_object(temp)
+        self.add_object(temp)
+        
+    def add_object_period(self, period, RN, mas):
+        temp = Object()
+        
+        M1 = 0;
+        M2 = numpy.array([0, 0, 0]);
+        for obj in self.objects:
+            M1 = M1 + obj.mas
+            M2 = M2 + obj.mas * obj.pos
+        
+        mass_pos = M2 / M1
+        
+        PosRad = numpy.abs( 0.5*(RN**2)*6.67E-11*M1*((period/numpy.pi)**2)**(1.0/3.0) )
+        VelRad = RN * numpy.sqrt(6.67E-11*2*M1/PosRad)
+        
+        AngleVer    = random() * 2.0 * numpy.pi
+        AngleAzi    = random() * 2.0 * numpy.pi
+        RandomVec   = numpy.array([numpy.cos(AngleVer) * numpy.cos(AngleAzi), numpy.cos(AngleVer) * numpy.sin(AngleAzi), numpy.sin(AngleVer)])
+        
+        temp.pos = PosRad * RandomVec + mass_pos
+        
+        AngleVer    = random() * 2.0 * numpy.pi
+        AngleAzi    = random() * 2.0 * numpy.pi
+        RandomVec   = numpy.array([numpy.cos(AngleVer) * numpy.cos(AngleAzi), numpy.cos(AngleVer) * numpy.sin(AngleAzi), numpy.sin(AngleVer)])
+        
+        VelocityVec = numpy.cross(temp.pos, RandomVec)
+        VelocityVec = VelocityVec / numpy.linalg.norm(VelocityVec)
+        VelocityVec = VelocityVec * VelRad
+        
+        temp.spd = VelocityVec
+        temp.mas = mas
+        
+        self.add_object(temp)
+        
+    def add_object_period2(self, period, Beta, mas):
+        temp = Object()
+        
+        M1 = 0;
+        M2 = numpy.array([0, 0, 0]);
+        for obj in self.objects:
+            M1 = M1 + obj.mas
+            M2 = M2 + obj.mas * obj.pos
+        
+        mass_pos = M2 / M1
+        
+        PosRad = (Beta*Beta*6.67E-11*M1*((0.5*period/numpy.pi)**2))**(1.0/3.0)
+        VelRad = 2.0 * numpy.pi * PosRad / period
+        
+        AngleVer    = random() * 2.0 * numpy.pi
+        AngleAzi    = random() * 2.0 * numpy.pi
+        RandomVec   = numpy.array([numpy.cos(AngleVer) * numpy.cos(AngleAzi), numpy.cos(AngleVer) * numpy.sin(AngleAzi), numpy.sin(AngleVer)])
+        
+        temp.pos = PosRad * RandomVec + mass_pos
+        
+        AngleVer    = random() * 2.0 * numpy.pi
+        AngleAzi    = random() * 2.0 * numpy.pi
+        RandomVec   = numpy.array([numpy.cos(AngleVer) * numpy.cos(AngleAzi), numpy.cos(AngleVer) * numpy.sin(AngleAzi), numpy.sin(AngleVer)])
+        
+        VelocityVec1 = numpy.cross(temp.pos, RandomVec)
+        VelocityVec1 = VelocityVec1 / numpy.linalg.norm(VelocityVec1)
+        
+        VelocityVec2 = (temp.pos - mass_pos) / numpy.linalg.norm(temp.pos - mass_pos)
+        
+        Rn1 = random()
+        Rn2 = random()
+        
+        VelocityVec1 = VelocityVec1 * Rn1
+        VelocityVec2 = VelocityVec2 * Rn2
+        
+        VelocityVec = VelocityVec1 + VelocityVec2
+        VelocityVec = VelocityVec / numpy.linalg.norm(VelocityVec)
+        
+        temp.spd = VelocityVec * VelRad
+        
+        temp.mas = mas
+        
+        self.add_object(temp)
+        
+        
+        
+        
+    def object_list(self):
+        for obj in self.objects:
+            obj.data()
+            print("\n\n")
         
         
     def Calc_Energy(self):
@@ -168,27 +276,26 @@ class ObjectSystem:
         return forces
     
     
-    def Calc_Iteration(self, dt = 1E+0):
+    def Calc_Iteration(self, dt):
         BegEnergy = self.Calc_Energy()
         
-        dt = 1E+0
         InterForces = self.Calc_Forces()
         
         for i in range(len(self.objects)):
             self.objects[i].spd = self.objects[i].spd + (InterForces[i] / self.objects[i].mas) * dt
-            self.objects[i].pos = self.objects[i].pos + self.objects[i].spd * dt
+            self.objects[i].pos = self.objects[i].pos + self.objects[i].spd * dt + 0.5 * (InterForces[i] / self.objects[i].mas) * dt * dt
             self.objects[i].update()
-            
+        
         EndEnergy = self.Calc_Energy()
         
         EnergyError = 2.0 * (BegEnergy - EndEnergy) / (BegEnergy + EndEnergy)
         
         assert EnergyError <= 1E+2
         
-    def Calc_time(self, Time, dt=1E+1):
+    def Calc_time(self, Time, dt):
         t = 0;
         
-        while t < Time:
+        while t <= Time:
             t = t + dt;
             self.Calc_Iteration(dt)
     
@@ -212,6 +319,33 @@ class ObjectSystem:
         animation = camera.animate(blit=False,interval=100)
         animation.save("filename.gif")
     
+    def Create_NoTrack_Animation(self, Step, End, Begin):
+        
+        colors = ["tab:blue", "tab:orange", "tab:green", "tab:red", "tab:purple", "tab:brown", "tab:pink", "tab:olive", "tab:gray", "tab:cyan"]
+        
+        X = [0, 0]
+        Y = [0, 0]
+        Z = [0, 0]
+        
+        for a in range(Begin, End, Step):
+            for i in range(len(self.objects)):
+                X[1] = X[0]
+                X[0] = self.objects[i].trajectoryX[a]
+                
+                Y[1] = Y[0]
+                Y[0] = self.objects[i].trajectoryY[a]
+                
+                Z[1] = Z[0]
+                Z[0] = self.objects[i].trajectoryZ[a]
+                
+                ax.scatter(X, Y, Z, s=40, color=colors[i%10])
+            
+            ax.view_init(elev=25, azim=45+a)
+            
+            camera.snap()
+        
+        animation = camera.animate(blit=False,interval=100)
+        animation.save("filename.gif")
     
     def draw(self, a = 25, b = -45):
         ax.clear()
@@ -222,44 +356,51 @@ class ObjectSystem:
         
         for i in range(len(self.objects)):
             ax.plot3D(self.objects[i].trajectoryX, self.objects[i].trajectoryY, self.objects[i].trajectoryZ,"--", color=colors[i%10], linewidth=1.5)
+            ax.scatter(self.objects[i].trajectoryX[::-1][:2], self.objects[i].trajectoryY[::-1][:2], self.objects[i].trajectoryZ[::-1][:2], s=40, color=colors[i%10])
             
         fig.canvas.draw()
     
     
     
+
     
 System = ObjectSystem()
 
-# System.add_object_by_parameters([0,0,0], [0,0,0], [0,0,1E-5], 1E+30, 1E+9)
-# System.add_object_by_parameters([0,1E+10,0], [1E+5,0,0], [0,1E-5,1E-5], 1E+20, 1E+5)
-# System.add_object_by_parameters([0,-1E+10,0], [-1E+5,0,0], [0,1E-5,1E-5], 1E+20, 1E+5)
+System.add_object_by_parameters([0,0,0], [0,0,0], [0,1,0], 2E+10,     1E+9)
 
-# System.add_object_by_parameters([0,0,0],        [0,0,0],        [0,0,1E-5],         1E+30, 1E+9)
-# System.add_object_by_parameters([0,1E+9,0],     [+1E+5,0,0],    [0,1E-5,1E-5],      1E+20, 1E+5)
-# System.add_object_by_parameters([0,-1E+9,0],    [-1E+5,0,0],    [0,1E-5,1E-5],      1E+30, 1E+5)
+System.add_object_period(30,  0.707, 7E+4)
+System.add_object_period(20,  0.9, 7E+4)
+System.add_object_period(30,  0.7, 7E+8)
 
-# System.add_object_by_parameters([0,0,0],    [0,0,0],            [0,0,1],    10000, 1)
-# System.add_object_by_parameters([0,+1,0],   [+81.7E-5,0,0],     [0,0,1],    10, 1)
-# System.add_object_by_parameters([0,-1,0],   [-81.7E-5,0,0],     [0,0,1],    10, 1)
+# System.add_object_by_parameters([+0.5,0,0], [0,+1.08,0], [0,1,0], 2E+10,     1E+9)
+# System.add_object_by_parameters([-0.5,0,0], [0,-1.08,0], [0,1,0], 2E+10,     1E+9)
 
-System.add_object_by_parameters([0,0,0],                        [0,0,0],                    [0,1,0],            1E+30,     1E+9)
-System.add_object_by_parameters([+1E+9,0,0],                    [0,+1.4E+5,1.4E+5],         [0,1,0],            0.5E+29,   1E+9)
-System.add_object_by_parameters([-1E+9,0,0],                    [0,-1E+5,0],                [0,1,0],            0.5E+20,   1E+9)
-#System.add_object_by_parameters([+0.2E+9,-0.2E+8,-1E+4],        [0,+1E+5,0],                [0,1,0],            0.5E+15,   1E+9)
+# System.add_object_period(5,   0.707, 1E+3)
+# System.add_object_period(10,  0.707, 5E+3)
+# System.add_object_period(20,  0.707, 4E+4)
+# System.add_object_period(30,  0.707, 7E+4)
+# System.add_object_period(60,  0.707, 6E+8)
 
 
+System.object_list()
 print("System Energy:  ", System.Calc_Energy())
 print("System Impulse: ", System.Calc_Impulse())
 
 
-System.Calc_time(200000)
+print("Angle:", angle(System.objects[1].pos, System.objects[1].spd))
+
+System.Calc_time(120, 0.002 )
 
 System.draw()
-
+   
 plt.show()
 
-System.Create_Animation(1000, 200000, 0)
+#System.Create_Animation(250, 30000, 0)
+System.Create_NoTrack_Animation(300, 60001, 0)
 
-print("Finish")
+print("System Energy:  ", System.Calc_Energy())
+print("System Impulse: ", System.Calc_Impulse())
+
+print("Finish", len(System.objects[0].trajectoryX))
 
 

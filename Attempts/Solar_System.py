f8f7eab8d03730547410a397beeac82ca743fd51
tiny-engineery <90619504+tiny-engineery@users.noreply.github.com>
2021-12-27 03:01:17 +0300

    Test Destruction
    Added test destruction and description of functions

--- Attempts/Solar_System.py
+++ Attempts/Solar_System.py
@@ -14,6 +14,18 @@ G = 6.67
 
 
 def Norm(vec):
+    """
+    
+
+    Parameters
+    ----------
+    vec : входной вектор.
+
+    Returns
+    -------
+    Норму вектора.
+
+    """
     return (np.sum(vec ** 2))**(1/2)
 
 
@@ -58,15 +70,44 @@ class CosmicBody():
 
     def distance(self, body):
         return (body.vec_p - self.vec_p)
-    
+
     def move(self, dt):
+        """
+        
+        
+        Parameters
+        ----------
+        dt : промежуток времени
+
+        Returns
+        -------
+        Меняет координату как если бы тело двигалось равномерно прямолинейно в течении dt
+
+        """
+
         self.vec_p = self.vec_p + self.vec_v * dt * self.exist
 
     def grav(self, dt, *bodies):
+        """
+        
+
+        Parameters
+        ----------
+        dt : промежуток времени.
+        *bodies : кортеж взаимодействующих тел.
+        M : масса конкретного тела.
+
+        Returns
+        -------
+        Изменяет скорость self по второму закону ньютона для self и *bodies
+
+        """
+
         for i in range(len(bodies)):
             M = bodies[i].getMass()
             r = self.distance(bodies[i])
-            self.vec_v = self.vec_v + G * M * r * dt / (Norm(self.vec_p) ** 3) * self.exist * bodies[i].exist
+            self.vec_v = self.vec_v + G * M * r * dt / \
+                (Norm(r) ** 3) * self.exist * bodies[i].exist
 
     def getMass(self):
         return self.mass
@@ -80,7 +121,7 @@ class CosmicBody():
 
 if __name__ == '__main__':
 
-    def Test_2DMotion_2objects():
+    def test_2DMotion_StarBody():
         """
         Discrete motion. During time dt moves with constant velosity. 
         Momentum changes according to the Gravitational Law.
@@ -117,16 +158,16 @@ if __name__ == '__main__':
             plt.plot(x, y, '--', linewidth=1)
 
     def test_2DMotion_2Bodies():
-        velosity = [(0.25, 0), (-0.25, 0)]
-        Ast1 = CosmicBody(10, (0, 100), velosity[0])
-        Ast2 = CosmicBody(10, (0,-100), velosity[1])
+        velosity = [(2, 0), (-3, 0)]
+        Ast1 = CosmicBody(12, (0, 0), velosity[0])
+        Ast2 = CosmicBody(16, (0, 5), velosity[1])
         x1 = [Ast1.getPosition()[0]]
         y1 = [Ast1.getPosition()[1]]
         x2 = [Ast2.getPosition()[0]]
         y2 = [Ast2.getPosition()[1]]
-        
-        dt = 0.2
-        n = 10000
+
+        dt = 0.01
+        n = 1000
         t = 0
         while t < dt*n:
             Ast1.grav(dt, Ast2)
@@ -138,16 +179,48 @@ if __name__ == '__main__':
             x2.append(Ast2.getPosition()[0])
             y2.append(Ast2.getPosition()[1])
 
-            if (Norm(Ast1.distance(Ast2)) <= 5):
+            if (Norm(Ast1.distance(Ast2)) <= 0.2):
                 Ast1.destroy()
                 Ast2.destroy()
 
             t = t + dt
+        plt.show()
         plt.plot(x1, y1, '--', linewidth=1)
-        plt.plot(x2, y2, '--', linewidth=1)       
-        
-        
-        
+        plt.plot(x2, y2, '--', linewidth=1)
+
+    def test_destruction():
+        velosity = [(0.1, 0), (-0.1, 0)]
+        Ast1 = CosmicBody(12, (0, 0), velosity[0])
+        Ast2 = CosmicBody(5, (0, 8), velosity[1])
+        x1 = [Ast1.getPosition()[0]]
+        y1 = [Ast1.getPosition()[1]]
+        x2 = [Ast2.getPosition()[0]]
+        y2 = [Ast2.getPosition()[1]]
+
+        dt = 0.001
+        n = 25036
+        t = 0
+        while t < dt*n:
+            Ast1.grav(dt, Ast2)
+            Ast2.grav(dt, Ast1)
+            Ast1.move(dt)
+            Ast2.move(dt)
+            x1.append(Ast1.getPosition()[0])
+            y1.append(Ast1.getPosition()[1])
+            x2.append(Ast2.getPosition()[0])
+            y2.append(Ast2.getPosition()[1])
+
+            if (Norm(Ast1.distance(Ast2)) <= 0.05):
+                Ast1.destroy()
+                Ast2.destroy()
+
+            t = t + dt
+        plt.show()
+        plt.plot(x1, y1, 'o--', linewidth=1)
+        plt.plot(x2, y2, 'o--', linewidth=1)
+        assert Ast1.exist == 0
+        print("Test Destruction is Ok")
+
     def test_Star():
         Sun = Star(35, (3, 9))
         assert Sun.getMass() == 35
@@ -162,7 +235,8 @@ if __name__ == '__main__':
         # print (Tesla_Roadster.getVelosity())
         print("Test Tesla Motorspots is Ok")
 
-    # Test_2DMotion_2objects()
-    test_2DMotion_2Bodies()
+    # test_2DMotion_StarBody()
+    # test_2DMotion_2Bodies()
+    # test_destruction()
     test_Star()
     test_Body()
